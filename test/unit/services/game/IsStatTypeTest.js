7ac45d1d13b502c5ba44bd9a815019139be7e84e
Ben Becker <bforbendetta@gmail.com>
2016-01-31 22:43:02 -0800

    Added which plays to test from test data for IsStatType()

--- test/unit/services/game/IsStatTypeTest.js
+++ test/unit/services/game/IsStatTypeTest.js
@@ -4,25 +4,140 @@ describe('IsStatType', function() {
 	var IsStatType,testData,ncbData;
 	var statTypePlays = {
 		"p":[
-			{id:0,value:false},
-			{id:1,value:true},
-			{id:4,value:false},
-			{id:0,primary:true,value:false},
-			{id:1,primary:true,value:true},
-			{id:4,primary:true,value:false}
+			{id:0,value:false}, //jump ball
+			{id:1,value:true}, //made 2
+			{id:4,value:false}, //missed 3
+			{id:32,value:true}, //made FT
+			{id:0,primary:true,value:false}, //jump ball
+			{id:1,primary:true,value:true}, //made 2
+			{id:4,primary:true,value:false}, //missed 3
+			{id:32,primary:true,value:true} //made FT
 		],
-		"sht":[],
-		"fg":[],
-		"3s":[],
-		"ft":[],
-		"r":[],
-		"a":[],
-		"b":[],
-		"tno":[],
-		"s":[],
-		"pf":[],
-		"to":[],
-		"top":[]
+		"sht":[
+			{id:0,value:false}, //jump ball
+			{id:1,value:true}, //made 2
+			{id:4,value:true}, //missed 3
+			{id:31,value:true}, //missed FT
+			{id:32,value:true}, //made FT
+			{id:0,primary:true,value:false}, //jump ball
+			{id:1,primary:true,value:true}, //made 2
+			{id:4,primary:true,value:false}, //missed 3
+			{id:31,primary:true,value:false}, //missed FT
+			{id:32,primary:true,value:true} //made FT
+		],
+		"fg":[
+			{id:0,value:false}, //jump ball
+			{id:1,value:true}, //made 2
+			{id:4,value:true}, //missed 3
+			{id:32,value:false}, //made FT
+			{id:0,primary:true,value:false}, //jump ball
+			{id:1,primary:true,value:true}, //made 2
+			{id:4,primary:true,value:false}, //missed 3
+			{id:32,primary:true,value:false} //made FT
+		],
+		"3s":[
+			{id:0,value:false}, //jump ball
+			{id:1,value:false}, //made 2
+			{id:4,value:true}, //missed 3
+			{id:11,value:true}, //made 3 assist
+			{id:32,value:false}, //made FT
+			{id:0,primary:true,value:false}, //jump ball
+			{id:1,primary:true,value:false}, //made 2
+			{id:4,primary:true,value:false}, //missed 3
+			{id:11,primary:true,value:true}, //made 3 assist
+			{id:32,primary:true,value:false} //made FT
+		],
+		"ft":[
+			{id:0,value:false}, //jump ball
+			{id:1,value:false}, //made 2
+			{id:4,value:false}, //missed 3
+			{id:31,value:true}, //missed FT
+			{id:32,value:true}, //made FT
+			{id:0,primary:true,value:false}, //jump ball
+			{id:1,primary:true,value:false}, //made 2
+			{id:4,primary:true,value:false}, //missed 3
+			{id:31,primary:true,value:false}, //missed FT
+			{id:32,primary:true,value:true} //made FT
+		],
+		"r":[
+			{id:0,value:false}, //jump ball
+			{id:1,value:false}, //made 2
+			{id:5,value:true}, //defensive rebound
+			{id:14,value:true}, //offensive rebound
+			{id:0,primary:true,value:false}, //jump ball
+			{id:1,primary:true,value:false}, //made 2
+			{id:5,primary:true,value:true}, //defensive rebound
+			{id:14,primary:true,value:false}, //offensive rebound
+		],
+		"a":[
+			{id:0,value:false}, //jump ball
+			{id:1,value:false}, //made 2
+			{id:11,value:true}, //made 3 assist
+			{id:0,primary:true,value:false}, //jump ball
+			{id:1,primary:true,value:false}, //made 2
+			{id:11,primary:true,value:true}, //made 3 assist
+		],
+		"b":[
+			{id:0,value:false}, //jump ball
+			{id:1,value:false}, //made 2
+			{id:13,value:true}, //block
+			{id:0,primary:true,value:false}, //jump ball
+			{id:1,primary:true,value:false}, //made 2
+			{id:13,primary:true,value:true}, //block
+		],
+		"tno":[
+			{id:0,value:false}, //jump ball
+			{id:1,value:false}, //made 2
+			{id:3,value:true}, //turnover
+			{id:6,value:true}, //turnover - steal
+			{id:0,primary:true,value:false}, //jump ball
+			{id:1,primary:true,value:false}, //made 2
+			{id:3,primary:true,value:true}, //turnover
+			{id:6,primary:true,value:true}, //turnover - steal
+		],
+		"s":[
+			{id:0,value:false}, //jump ball
+			{id:1,value:false}, //made 2
+			{id:6,value:false}, //turnover - steal
+			{id:7,value:true}, //steal
+			{id:0,primary:true,value:false}, //jump ball
+			{id:1,primary:true,value:false}, //made 2
+			{id:3,primary:true,value:false}, //turnover
+			{id:6,primary:true,value:false}, //turnover - steal
+			{id:7,primary:true,value:true} //steal
+		],
+		"pf":[
+			{id:0,value:false}, //jump ball
+			{id:1,value:false}, //made 2
+			{id:2,value:true}, //floor foul
+			{id:29,value:true}, //shooting foul
+			{id:0,primary:true,value:false}, //jump ball
+			{id:1,primary:true,value:false}, //made 2
+			{id:2,primary:true,value:false}, //floor foul
+			{id:29,primary:true,value:true} //shooting foul
+		],
+		"to":[
+			{id:0,value:false}, //jump ball
+			{id:1,value:false}, //made 2
+			{id:30,value:true}, //timeout
+			{id:0,primary:true,value:false}, //jump ball
+			{id:1,primary:true,value:false}, //made 2
+			{id:30,primary:true,value:true} //timeout
+		],
+		//this really tests the data more than anything, so it's not worth much
+		"top":[
+			{id:0,value:true}, //jump ball
+			{id:1,value:true}, //made 2
+			{id:2,value:true}, //floor foul (new shot clock)
+			{id:3,value:true}, //turnover
+			{id:4,value:false}, //missed 3
+			{id:5,value:true}, //defensive rebound
+			{id:7,value:true}, //steal
+			{id:14,value:true}, //offensive rebound
+			{id:29,value:false}, //shooting foul (for purpose of shot clock stuff)
+			{id:31,value:false}, //missed FT first (1/2)
+			{id:32,value:true} //made FT second (2/2)
+		]
 	}
 
 	beforeEach(module('ssServices'));
@@ -59,9 +174,9 @@ describe('IsStatType', function() {
 							statType: ncbData.pl[statTypesI],
 							primary: statTypePlays[st][playI].primary
 						}
-					)
+					)+st+statTypePlays[st][playI].id
 				)
-				.toBe(statTypePlays[st][playI].value);
+				.toBe(statTypePlays[st][playI].value+st+statTypePlays[st][playI].id);
 			}
 		}
 	});
